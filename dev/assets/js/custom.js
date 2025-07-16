ac99298561700399de52f52f57f98c076d43e311
Ilya Poleshko <IlyaPoleshko@MacBook-Air.local>
2025-07-16 18:50:38 +0300

    feat: created an input and a checkbox

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -26,3 +26,9 @@ const banner = document.querySelector(".swiper-banner");
 const headerHeight = header.offsetHeight;
 
 banner.setAttribute("style", "--header-height: " + headerHeight + "px");
+
+const inputCheckbox = document.querySelectorAll('input[type="checkbox"]');
+
+inputCheckbox.forEach((item) => {
+	item.closest(".input-box").classList.add("input-box_checkbox");
+});
