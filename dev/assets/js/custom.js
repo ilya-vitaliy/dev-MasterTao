bc45007a5bdc97890974b652733abfb123fb1275
Ilya Poleshko <IlyaPoleshko@MacBook-Air.local>
2025-08-03 14:50:00 +0300

    feat: added bg-accent overlay

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -7,7 +7,6 @@ btnBurger.addEventListener("click", () =>
 );
 
 const swiperBanner = new Swiper(".swiper-banner", {
-	// Optional parameters
 	direction: "horizontal",
 	loop: true,
 	spaceBetween: 24,
@@ -17,7 +16,6 @@ const swiperBanner = new Swiper(".swiper-banner", {
 		prevEl: ".swiper-button-prev",
 	},
 
-	// If we need pagination
 	pagination: {
 		clickable: true,
 		el: ".swiper-pagination",
@@ -47,3 +45,33 @@ fileInput.forEach((item) => {
 
 const buttons = document.querySelectorAll("button");
 console.log(buttons);
+
+const advantagesSlideLength = document.querySelectorAll(
+	".advantages .swiper-slide",
+).length;
+
+const swiperAdvantages = new Swiper("#advantages", {
+	spaceBetween: 12,
+	loop: true,
+	simulateTouch: true,
+	slidesPerView: 1,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+		enabled: true,
+	},
+
+	breakpoints: {
+		1025: {
+			spaceBetween: 0,
+			slidesPerView: advantagesSlideLength,
+			slidesPerGroup: advantagesSlideLength,
+			loop: false,
+			simulateTouch: false,
+			pagination: {
+				enabled: false,
+				el: ".swiper-pagination",
+			},
+		},
+	},
+});
